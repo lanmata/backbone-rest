19c029a84fdd5568e7f6a900a67c61c8f18d8525
Luis Mata <luis.antonio.mata@gmail.com>
2020-09-02 05:23:21 -0400

    Correcciones para carga remota de configuración

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,13 @@ LABEL version="1.0"
 LABEL description="Componente para configuración de servicios"
 LABEL mantainer="Luis Mata luis.antonio.mata@gmail.com"
 
-ARG JAR_FILE=target/prx-backbone-rest.jar
-RUN addgroup -S appmng && adduser -S jvapps -G appmng
-USER jvapps:appmng
-
-EXPOSE 8083
+#RUN addgroup -S appmng && adduser -S jvapps -G appmng
+#USER jvapps:appmng
 WORKDIR /usr/local/runme
+ARG JAR_FILE=target/prx-backbone-rest.jar
 COPY ${JAR_FILE} "prx-backbone-rest.jar"
-COPY src/main/resources/prx_backbone_rest.jks prx_backbone_rest.jks
+COPY prx_backbone_rest.jks prx_backbone_rest.jks
+COPY prx_backbone_rest_discovery.jks prx_backbone_rest_discovery.jks
 
+EXPOSE 8085
 ENTRYPOINT ["java", "-jar", "prx-backbone-rest.jar"]
--- src/main/java/com/prx/backoffice/PrxBackofficeRestApplication.java
+++ src/main/java/com/prx/backoffice/PrxBackofficeRestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -16,7 +17,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableDiscoveryClient
 @EnableJpaRepositories(basePackages = {"com.prx.persistence"})
 @EntityScan(basePackages = {"com.prx.persistence"})
-@SpringBootApplication(scanBasePackages = {"com.prx.backoffice","com.prx.commons.config"})
+@ComponentScan(basePackages = {"com.prx.backoffice","com.prx.commons.config"})
+@SpringBootApplication
 public class PrxBackofficeRestApplication  extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(PrxBackofficeRestApplication.class, args);
