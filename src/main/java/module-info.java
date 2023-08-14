952dceb150c09ac60d5f1531b178aba9902228e3
Luis Mata <69470188+lanmata@users.noreply.github.com>
2023-08-14 01:30:31 -0400

    PRX-121 API - List by Role Id and status - Fixed surefire dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module prx.backbone.rest {
     exports com.prx.backoffice;
+    exports com.prx.backoffice.enums.keys;
     exports com.prx.backoffice.converter to spring.beans;
     exports com.prx.backoffice.loggers.config to spring.beans;
     exports com.prx.backoffice.loggers.services to spring.beans, spring.core;
@@ -82,6 +83,5 @@ module prx.backbone.rest {
     opens com.prx.backoffice.v1.roles.api.controller to spring.core;
     opens com.prx.backoffice.v1.roles.api.to to spring.beans, spring.core;
     opens com.prx.backoffice.v1.report.api.controller to spring.core;
-    exports com.prx.backoffice.enums.keys;
 
 }
