9a67143d8c4f7ff8720ccf369ffed38a0561cb9f
Luis Mata <69470188+lanmata@users.noreply.github.com>
2023-11-06 03:37:43 -0500

    PRX-206 API - Implement Service monitor client - Updated dependencies in the pom.xml file - Added @EnableDiscoveryClient in the PrxBackofficeRestApplication class. - Deleted certificates not required.

--- src/main/java/com/prx/backoffice/PrxBackofficeRestApplication.java
+++ src/main/java/com/prx/backoffice/PrxBackofficeRestApplication.java
@@ -16,13 +16,10 @@
 package com.prx.backoffice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
-import org.springframework.cloud.openfeign
-		.FeignClient;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -33,6 +30,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  * @version 1.0.1.20200904-01, 26-10-2020
  */
 @FeignClient
+@EnableDiscoveryClient
 @SpringBootApplication
 @EntityScan(basePackages = {"com.prx.persistence"})
 @EnableJpaRepositories(basePackages = {"com.prx.persistence"})
