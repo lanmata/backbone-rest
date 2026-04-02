015a6a6f76762b54f905cd82160112f7bd55638d
Luis Mata <lantmata@gmail.com>
2026-04-02 04:53:27 -0400

    Implement Cloudflare R2 storage integration for profile images

--- src/main/java/com/prx/backoffice/security/config/SecurityConfig.java
+++ src/main/java/com/prx/backoffice/security/config/SecurityConfig.java
@@ -27,7 +27,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
@@ -125,6 +124,6 @@ public class SecurityConfig {
     @Bean
     public RestTemplate getRestTemplate() throws CertificateSecurityException {
         RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
-        return restTemplateBuilder.setSslBundle(keyStoreUtil.getSslBundle(securityProperties)).build();
+        return restTemplateBuilder.sslBundle(keyStoreUtil.getSslBundle(securityProperties)).build();
     }
 }
