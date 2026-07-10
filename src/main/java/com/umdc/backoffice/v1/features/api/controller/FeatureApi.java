ea7e0198d6ed4efe20efc1056d088bbe0d93de06
Luis Mata <prx.innova@gmail.com>
2026-07-10 00:39:45 -0400

    feat(RoleApi): add features linking to role creation and update API documentation

--- src/main/java/com/umdc/backoffice/v1/features/api/controller/FeatureApi.java
+++ src/main/java/com/umdc/backoffice/v1/features/api/controller/FeatureApi.java
@@ -106,7 +106,7 @@ public interface FeatureApi {
             @ApiResponse(responseCode = HttpStatusUtil.CREATED_STR, description = "OK"),
             @ApiResponse(responseCode = HttpStatusUtil.NOT_ACCEPTABLE_STR, description = "NOT ACCEPTABLE")
     })
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, path = "/")
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     default ResponseEntity<Feature> create(@Parameter(description = "Feature properties", required = true) @RequestBody FeatureRequest featureRequest) {
         return getService().create(featureRequest.getFeature());
     }
